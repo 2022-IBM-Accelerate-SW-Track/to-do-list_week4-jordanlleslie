bef8e156ea8d7bfab39cdf3849e4fd3cc8442e3b
Jordan Leslie <jordan02leslie@gmail.com>
2022-06-30 09:24:15 -0400

    delete typo from AddTodo.js

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -11,7 +11,7 @@ class AddTodo extends Component {
     this.state = {
       content: "",
       date: "",
-      duedate: null``
+      duedate: null
     };
   }
   // The handleChange function updates the react state with the new input value provided from the user and the current date/time.
